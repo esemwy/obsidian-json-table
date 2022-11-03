4d267c06be785bd039c0c384fc861f8e97494deb
Esemwy <github.5.smy@neverbox.com>
2022-11-03 19:39:44 -0400

    render JSON to table

---
+++ render.ts
@@ -0,0 +1,43 @@
+import { MarkdownRenderChild } from 'obsidian';
+
+export class TableRenderer extends MarkdownRenderChild {
+  constructor(
+    public caption: string, public headers: string[],
+    public rows: any[], public container: HTMLElement) {
+    super(container)
+  }
+
+  async onload() {
+    await this.render()
+  }
+
+  async render() {
+    const tableEl = this.container.createEl('table');
+    tableEl.createEl('caption', { text: this.caption });
+
+    const theadEl = tableEl.createEl('thead');
+    const tbodyEl = tableEl.createEl('tbody');
+
+    for (const headerLine of this.headers) {
+      const headerEl = theadEl.createEl('tr');
+      for (const header of headerLine) {
+        const thEl = headerEl.createEl('th');
+        thEl.createEl('b', { text: header });
+      }
+    }
+
+    for (const row of this.rows) {
+      const trEl = tbodyEl.createEl('tr');
+
+      for (const col of row) {
+        trEl.createEl('td', { text: col });
+      }
+    }
+  }
+}
+
+export function renderErrorPre(container: HTMLElement, error: string): HTMLElement {
+  let pre = container.createEl('pre', { cls: ["json-table", "json-error"] });
+  pre.appendText(error);
+  return pre;
+}
